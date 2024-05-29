aee34fe7f1ed90175adb58136267504292ae22fa
Koren <korenkpalan96@gmail.com>
2024-05-29 09:20:07 +0300

    Web scraping works

--- logic/web_scraping/google_jobs_scraping.py
+++ logic/web_scraping/google_jobs_scraping.py
@@ -1,6 +1,5 @@
 """ this module is dedicated to script job listings from google jobs"""
 import time
-import requests
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -13,6 +12,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import DriverManager
+from decorators import messure_function_time_dec
 
 
 # region constants
@@ -32,79 +32,72 @@ class GoogleJobsTimePeriod(Enum):
 #     except Exception as e:
 #         print(e)
 #         return False
-def click_button(xpath, _driver) -> bool:
+def click_button(xpath, _driver, timeout=1.0) -> bool:
     try:
         # Wait for the button to be present in the DOM
-        button = WebDriverWait(_driver, 2).until(EC.presence_of_element_located((By.XPATH, xpath)))
+        button = WebDriverWait(_driver, timeout).until(EC.presence_of_element_located((By.XPATH, xpath)))
 
         # Once present, wait for it to be clickable
-        WebDriverWait(_driver, 2).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+        WebDriverWait(_driver, timeout).until(EC.element_to_be_clickable((By.XPATH, xpath)))
 
         # Click the button
         button.click()
         return True
     except TimeoutException as e:
-        print("Timed out waiting for button to be present or clickable:", e)
+        error = f"Timed out waiting for button to be present or clickable: {e}"
+        write_text_to_file('click_button_logs.txt', 'a', error, 400 * "=")
         return False
     except NoSuchElementException as e:
-        print("Button not found:", e)
+        error = f"Button not found: {e}"
+        write_text_to_file('click_button_logs.txt', 'a', error, 400 * "=")
         return False
     except ElementClickInterceptedException as e:
-        print("Button click intercepted:", e)
+        error = f"Button click intercepted: {e}"
+        write_text_to_file('click_button_logs.txt', 'a', error, 400 * "=")
         return False
     except Exception as e:
-        print("An error occurred:", e)
+        error = f"An error occurred: {e}"
+        write_text_to_file('click_button_logs.txt', 'a', error, 400 * "=")
         return False
 
 
 def get_full_description(xpath, _driver):
     try:
         # Wait for the element to be present in the DOM
-        element = WebDriverWait(_driver, 1).until(EC.presence_of_element_located((By.XPATH, xpath)))
-
+        element = WebDriverWait(_driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
+    except TimeoutException as e:
+        print("Timed out waiting for element to be present:", e)
+    try:
         # Once present, wait for it to be visible
-        WebDriverWait(_driver, 1).until(EC.visibility_of(element))
+        WebDriverWait(_driver, 10).until(EC.visibility_of(element))
+    except TimeoutException as e:
+        print("Timed out waiting for element to be visible:", e)
 
+    try:
         # Retrieve and return the text of the element
-        _description = element.text
-        if _description:
-            return _description
+        description = element.text
+        if description:
+            return description
         else:
             print("Element is present but contains no text.")
-            return None
-    except TimeoutException as e:
-        print("Timed out waiting for element to be present or visible:", e)
-        return None
-    except NoSuchElementException as e:
-        print("Element not found:", e)
-        return None
     except Exception as e:
         print("An error occurred:", e)
-        return None
 
 
 def write_text_to_file(filepath, mode, text, seperator, encoding='utf-8'):
     with open(filepath, mode, encoding=encoding) as file:
-        file.write(text + '/n')
+        file.write(text + '\n')
         if seperator:
-            file.write(seperator + '/n')
+            file.write(seperator + '\n')
 
 
 # region params
-# Set Chrome options
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--start-maximized")
-chrome_options.add_argument("--disable-extensions")
-
-# Set location to Israel
-chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US',
-                                                 'profile.default_content_setting_values.geolocation': 2})
 # Define the search key and time period
 search_key = 'backend developer'.replace(' ', '+')
-time_period = "month"
+time_period = GoogleJobsTimePeriod.MONTH.value
 # Construct the base URL
 base_url = (f"""
-https://www.google.com/search?q={search_key}+jobs+israel&ibp=htl;jobs&hl=en&gl=us#fpstate=tldetail&=&=&htivrt=jobs&htichips=date_posted:month&htischips=date_posted;month&htidocid=nPmFkUrgbES8cLN1AAAAAA%3D%3D
+https://www.google.com/search?q={search_key}+jobs+israel&ibp=htl;jobs&hl=en&gl=us#fpstate=tldetail&=&=&htivrt=jobs&htichips=date_posted:{time_period}&htischips=date_posted;{time_period}&htidocid=nPmFkUrgbES8cLN1AAAAAA%3D%3D
 """)
 print(base_url)
 
@@ -117,13 +110,16 @@ params = {
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.execute_cdp_cmd("Page.setGeolocationOverride", params)
 driver.get(base_url)
-# endregion
 
 
 wait = WebDriverWait(driver, 10)
 button_full_xpath = '/html/body/div[2]/div/div[2]/div[1]/div/div/div[3]/div[2]/div/div[1]/div/div/div[4]/g-expandable-container/div/div/div/div/div/div/g-expandable-content/span/div/g-inline-expansion-bar/div[1]/div'
 expandable_text_full_xpath = '/html/body/div[2]/div/div[2]/div[1]/div/div/div[3]/div[2]/div/div[1]/div/div/div[4]/g-expandable-container/div/div/div/span'
 text_full_xpath = '/html/body/div[2]/div/div[2]/div[1]/div/div/div[3]/div[2]/div/div[1]/div/div/div[4]/g-expandable-container/div/div/span'
+# endregion
+
+
+
 # Wait for the job listings to appear
 try:
     job_listings = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "li")))
@@ -137,6 +133,7 @@ print('total: ', len(job_listings))
 skip_amount = 0
 previous_size = -1
 inserted_descriptions = 0
+button_not_found_count = 0
 while job_listings:
     # If the previous size is the same as the current size, there are no more job listings to load
     if previous_size == len(job_listings):
@@ -147,21 +144,20 @@ while job_listings:
     for i in range(skip_amount, len(job_listings)):
         # Scroll to view
         driver.execute_script("arguments[0].scrollIntoView();", job_listings[i])
-        description = None
         # Click on the listing
         job_listings[i].click()
+
         try:
-            is_clicked = click_button(button_full_xpath, driver)
+            is_clicked = click_button(button_full_xpath, driver, timeout=0.5)
             if is_clicked:
                 description = get_full_description(expandable_text_full_xpath, driver)
             else:
                 description = get_full_description(text_full_xpath, driver)
+                button_not_found_count += 1
 
-            if description is not None:
+            if description:
                 inserted_descriptions += 1
                 write_text_to_file('descriptions.txt', 'a', description, 400 * "=")
-
-
 
         except NoSuchElementException:
             print("Target element not found!")
@@ -171,4 +167,4 @@ while job_listings:
 
     # Refind the job listings to avoid StaleElementReferenceException
     job_listings = driver.find_elements(By.CSS_SELECTOR, "li")
-print(f"Total Inserted: {inserted_descriptions} \n Total Failed: {len(job_listings) - inserted_descriptions}")
+print(f"Inserted descriptions: {inserted_descriptions}, job_listings:{len(job_listings)}, button not found: {button_not_found_count}")
--- logic/web_scraping/test.py
+++ logic/web_scraping/test.py
@@ -2,6 +2,7 @@
 import time
 import requests
 from bs4 import BeautifulSoup
+from lxml import etree
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -51,14 +52,14 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 driver.execute_cdp_cmd("Page.setGeolocationOverride", params)
 driver.get(base_url)
 
-
-# endregion
-button_xpath = '//*[@id="_yNtVZtziFeOqkdUPtYyw4A8_6"]'
-button_full_xpath = '/html/body/div[2]/div/div[2]/div[1]/div/div/div[3]/div[2]/div/div[1]/div/div/div[4]/g-expandable-container/div/div/div/div/div/div/g-expandable-content/span/div/g-inline-expansion-bar/div[1]/div'
-expandable_text_full_xpath = '/html/body/div[2]/div/div[2]/div[1]/div/div/div[3]/div[2]/div/div[1]/div/div/div[4]/g-expandable-container/div/div/div/span'
-text_full_xpath = '/html/body/div[2]/div/div[2]/div[1]/div/div/div[3]/div[2]/div/div[1]/div/div/div[4]/g-expandable-container/div/div/span'
-text_xpath = '//*[@id="gws-plugins-horizon-jobs__job_details_page"]/div/div[4]/g-expandable-container/div/div/span'
-wait = WebDriverWait(driver, 10)
+#
+# # endregion
+# button_xpath = '//*[@id="_yNtVZtziFeOqkdUPtYyw4A8_6"]'
+# button_full_xpath = '/html/body/div[2]/div/div[2]/div[1]/div/div/div[3]/div[2]/div/div[1]/div/div/div[4]/g-expandable-container/div/div/div/div/div/div/g-expandable-content/span/div/g-inline-expansion-bar/div[1]/div'
+# expandable_text_full_xpath = '/html/body/div[2]/div/div[2]/div[1]/div/div/div[3]/div[2]/div/div[1]/div/div/div[4]/g-expandable-container/div/div/div/span'
+# text_full_xpath = '/html/body/div[2]/div/div[2]/div[1]/div/div/div[3]/div[2]/div/div[1]/div/div/div[4]/g-expandable-container/div/div/span'
+# text_xpath = '//*[@id="gws-plugins-horizon-jobs__job_details_page"]/div/div[4]/g-expandable-container/div/div/span'
+# wait = WebDriverWait(driver, 10)
 
 
 def click_button(xpath, _driver) -> bool:
@@ -86,28 +87,47 @@ def click_button(xpath, _driver) -> bool:
         return False
 
 
-
-
 def get_full_description(xpath, _driver):
     try:
         # Wait for the element to be present in the DOM
         element = WebDriverWait(_driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
-
+    except TimeoutException as e:
+        print("Timed out waiting for element to be present:", e)
+    try:
         # Once present, wait for it to be visible
         WebDriverWait(_driver, 10).until(EC.visibility_of(element))
+    except TimeoutException as e:
+        print("Timed out waiting for element to be visible:", e)
 
+    try:
         # Retrieve and return the text of the element
         description = element.text
         if description:
             return description
         else:
             print("Element is present but contains no text.")
-    except TimeoutException as e:
-        print("Timed out waiting for element to be present or visible:", e)
-    except NoSuchElementException as e:
-        print("Element not found:", e)
     except Exception as e:
         print("An error occurred:", e)
+
+
+def get_span_text(url, xpath):
+  response = requests.get(url)
+  if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'lxml')
+    # Check for successful parsing
+    if soup.title:
+      dom = etree.HTML(str(soup))
+      element = dom.xpath(xpath)
+      if element:
+        return element[0].text
+      else:
+        return "Element not found"
+    else:
+      return "Failed to parse HTML content"
+  else:
+    return f"Failed to retrieve the page. Status code: {response.status_code}"
+
+
 
 button_xpath = '//*[@id="_yNtVZtziFeOqkdUPtYyw4A8_6"]'
 button_full_xpath = '/html/body/div[2]/div/div[2]/div[1]/div/div/div[3]/div[2]/div/div[1]/div/div/div[4]/g-expandable-container/div/div/div/div/div/div/g-expandable-content/span/div/g-inline-expansion-bar/div[1]/div'
@@ -116,3 +136,4 @@ text_full_xpath = '/html/body/div[2]/div/div[2]/div[1]/div/div/div[3]/div[2]/div
 text_xpath = '//*[@id="gws-plugins-horizon-jobs__job_details_page"]/div/div[4]/g-expandable-container/div/div/span'
 
 print(get_full_description(text_full_xpath, driver))
+# print(get_span_text(base_url, text_full_xpath))
