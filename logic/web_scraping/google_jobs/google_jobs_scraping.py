77a1eab1c8372cf529600037cd9dc49f048b27c2
Koren <korenkpalan96@gmail.com>
2024-05-29 14:23:02 +0300

    functionality checked

---
+++ logic/web_scraping/google_jobs/google_jobs_scraping.py
@@ -0,0 +1,278 @@
+""" this module is dedicated to script job listings from google jobs"""
+import time
+from datetime import datetime
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, StaleElementReferenceException, \
+    ElementClickInterceptedException
+from enum import Enum
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+from decorators.decorator_measure_function_time import measure_function_time
+import os
+from dotenv import load_dotenv
+
+"""
+- Add docstring 
+- remove unnecessary file writing and imports
+- make it a class
+- check in main pipeline
+"""
+load_dotenv()
+
+
+def countdown(n):
+    for i in range(n, 0, -1):
+        print(f"\r{i}", end="", flush=True)
+        time.sleep(1)
+    print("\r0", flush=True)  # Ensure the last number is also printed on the same line
+
+
+
+class GoogleJobsTimePeriod(Enum):
+    TODAY = 'today'
+    THREE_DAYS = '3days'
+    WEEK = 'week'
+    MONTH = 'month'
+
+
+def click_button(xpath, _driver, timeout=1.0) -> (bool, str):
+    try:
+        # Wait for the button to be present in the DOM
+        button = WebDriverWait(_driver, timeout).until(EC.presence_of_element_located((By.XPATH, xpath)))
+
+        # Once present, wait for it to be clickable
+        WebDriverWait(_driver, timeout).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+
+        # Click the button
+        button.click()
+        return True, ''
+    except TimeoutException as e:
+        error = f"click_button() -> Timed out waiting for button to be present or clickable"
+        return False, error
+    except NoSuchElementException as e:
+        error = f"click_button() -> Button not found"
+        return False, error
+    except ElementClickInterceptedException as e:
+        error = f"click_button() -> Button click intercepted"
+        return False, error
+
+
+def get_full_description(xpath, _driver) -> (bool, str):
+    try:
+        # Wait for the element to be present in the DOM
+        element = WebDriverWait(_driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
+    except TimeoutException as e:
+        error = f"get_full_description() -> Timed out waiting for element to be present"
+        return False, error
+    try:
+        # Once present, wait for it to be visible
+        WebDriverWait(_driver, 10).until(EC.visibility_of(element))
+    except TimeoutException as e:
+        error = f"get_full_description() -> Timed out waiting for element to be visible"
+        return False, error
+
+    try:
+        # Retrieve and return the text of the element
+        description = element.text
+        if description:
+            return True, description
+        else:
+            error = "get_full_description() -> Element is present but contains no text."
+            return False, error
+    except Exception as e:
+        error = f"get_full_description() -> An error occurred: {e}"
+        return False, error
+
+
+def write_text_to_file(filepath: str, mode: str, text: str, seperator_sign='=', seperator_length=300, encoding='utf-8',
+                       add_time_stamp=True) -> bool:
+    try:
+        seperator = seperator_length * seperator_sign
+        with open(filepath, mode, encoding=encoding) as file:
+            if add_time_stamp:
+                timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                file.write(f"({timestamp}) -> {text}\n")
+            else:
+                file.write(f"{text} \n")
+            if seperator:
+                file.write(seperator + '\n')
+        return True
+    except Exception as e:
+        print("An error occurred: ", e)
+        return False
+
+
+def setup_chrome_driver(params=None, set_auto_params=True, activate=False, url='', headless=True) -> WebDriver:
+    if params is None and set_auto_params is True:
+        params = {
+            "latitude": 32.109333,
+            "longitude": 34.855499,
+            "accuracy": 100
+        }
+
+    if headless:
+        chrome_options = Options()
+        chrome_headless_arguments = ('--headless', '--no-sandbox', '--disable-dev-shm-usage', "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" )
+        for option in chrome_headless_arguments:
+            chrome_options.add_argument(option)
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    else:
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        driver.execute_cdp_cmd("Page.setGeolocationOverride", params)
+    if activate:
+        if len(url) > 0:
+            driver.get(url)
+        else:
+            print('Could not activate driver url is necessary')
+
+    if activate is False and len(url) > 0:
+        print('Could not activate driver: activate is set to False')
+
+    return driver
+
+
+def build_google_jobs_url(search_value: str, googleJobsTimePeriod: GoogleJobsTimePeriod, replace_with_char='+') -> str:
+    snake_case_search_value = search_value.replace(' ', replace_with_char)
+    time_period = googleJobsTimePeriod.value
+    url = (f"""
+    https://www.google.com/search?q={snake_case_search_value}+jobs+israel&ibp=htl;jobs&hl=en&gl=us#fpstate=tldetail&=&=&htivrt=jobs&htichips=date_posted:{time_period}&htischips=date_posted;{time_period}
+    """)
+    return url
+
+
+def setup_web_driver_wait(driver: WebDriver, timeout=10) -> WebDriverWait[WebDriver | WebElement]:
+    return WebDriverWait(driver, timeout)
+
+
+
+def get_job_listings(driver: WebDriver, wait: WebDriverWait, expand_job_description_button_xpath: str,
+                     expandable_job_description_xpath: str,
+                     not_expanded_job_description_xpath: str, click_button_timeout=1.0) -> list[str]:
+    try:
+        job_listings = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "li")))
+    except TimeoutException:
+        write_text_to_file('../web_scrape_main_run_log.txt', 'a', "Job listings did not appear within the timeout period.")
+        driver.quit()
+        return []
+
+    skip_amount = 0
+    previous_size = -1
+    inserted_descriptions = 0
+    job_listings_result_list = []
+    urls_attempted_set = set()
+    while job_listings:
+        # If the previous size is the same as the current size, there are no more job listings to load
+        if previous_size == len(job_listings):
+            break
+
+        previous_size = len(job_listings)
+        # Iterate through each job listing, skipping the previously clicked ones
+        for i in range(skip_amount, len(job_listings)):
+            try:
+                # Scroll to view
+                driver.execute_script("arguments[0].scrollIntoView();", job_listings[i])
+                # Click on the listing
+                job_listings[i].click()
+
+
+                listing_url = driver.current_url
+                if listing_url in urls_attempted_set:
+                    write_text_to_file('../web_scrape_main_run_log.txt', 'a',
+                                       f"Repeated this url(No. {i}): {listing_url}")
+                else:
+                    urls_attempted_set.update(listing_url)
+
+
+            except StaleElementReferenceException:
+                write_text_to_file('../web_scrape_main_run_log.txt', 'a',
+                                   'StaleElementReferenceException: Element no longer exists in the dom problem fetching list to soon error')
+                driver.quit()
+                return []
+
+            try:
+                is_clicked, result_message_click_button = click_button(expand_job_description_button_xpath, driver, timeout=click_button_timeout)
+                if is_clicked:
+                    is_successful, description = get_full_description(expandable_job_description_xpath, driver)
+                else:
+                    write_text_to_file('../web_scrape_main_run_log.txt', 'a', result_message_click_button)
+                    is_successful, description = get_full_description(not_expanded_job_description_xpath, driver)
+
+                if is_successful:
+                    inserted_descriptions += 1
+                    job_listings_result_list.append(description)
+                else:
+                    write_text_to_file('../web_scrape_main_run_log.txt', 'a', description)
+
+            except NoSuchElementException:
+                write_text_to_file('../web_scrape_main_run_log.txt', 'a',
+                                   'NoSuchElementException: did not find button or description check logs')
+
+            # Update the skip amount for the next iteration
+            skip_amount = len(job_listings)
+
+        # Refind the job listings to avoid StaleElementReferenceException
+        job_listings = driver.find_elements(By.CSS_SELECTOR, "li")
+
+    text = f"Inserted descriptions: {inserted_descriptions}, job_listings:{len(job_listings)}"
+    write_text_to_file('../web_scrape_main_run_log.txt', 'a', text)
+
+    return job_listings_result_list
+
+
+def main():
+    # region params
+    # Define the search key and time period
+    search_value = 'backend developer'
+    time_period = GoogleJobsTimePeriod.MONTH
+    button_full_xpath = os.environ.get('SHOME_FULL_DESCRIPTION_BUTTON_XPATH_GOOGLE_JOBS')
+    expandable_text_full_xpath = os.environ.get('EXPANDABLE_JOB_DESCRIPTION_TEXT_XPATH_GOOGLE_JOBS')
+    not_expandable_text_full_xpath = os.environ.get('NOT_EXPANDABLE_JOB_DESCRIPTION_TEXT_XPATH_GOOGLE_JOBS')
+
+
+    listings_list = []
+    interval_attempts = 0
+    max_interval_attempts = 10
+    sleep_time_between_attempt_in_seconds = 30
+    is_success = False
+    url = build_google_jobs_url(search_value, time_period)
+    driver = setup_chrome_driver(url=url, activate=True)
+    wait = setup_web_driver_wait(driver, 3)
+    # endregion
+
+    # create intervals
+    while is_success is False and interval_attempts < max_interval_attempts:
+        if interval_attempts > 0:
+            print("Interval attempt failed retry in: ")
+            countdown(sleep_time_between_attempt_in_seconds)
+            driver = setup_chrome_driver(url=url, activate=True)
+            wait = setup_web_driver_wait(driver, 10)
+
+        interval_attempts += 1
+        text = 'Attempt number: %d' % interval_attempts
+        write_text_to_file('../web_scrape_main_run_log.txt', 'a', text)
+        listings_list = get_job_listings(driver, wait, button_full_xpath,
+                                         expandable_text_full_xpath,
+                                         not_expandable_text_full_xpath, 1)
+
+        if len(listings_list) > 0:
+            is_success = True
+
+    if is_success is True:
+        for job in listings_list:
+            write_text_to_file('../descriptions.txt', 'a', job)
+    else:
+        text = 'Failed to scrape job listings after maximum attempts(%d)' % max_interval_attempts
+        write_text_to_file('../web_scrape_main_run_log.txt', 'a', text)
+
+
+
+if __name__ == '__main__':
+    main()
+
