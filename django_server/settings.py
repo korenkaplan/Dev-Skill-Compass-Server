79758ec7fe5523b12940e18e200b3c515f82ce1a
Koren <korenkpalan96@gmail.com>
2024-06-19 15:39:44 +0300

    changed internal ips

--- django_server/settings.py
+++ django_server/settings.py
@@ -34,7 +34,7 @@ DEBUG = os.environ.get("DEBUG") == "true"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
-INTERNAL_IPS = ['127.0.0.1', 'https://dev-skill-compass-server.onrender.com']
+INTERNAL_IPS = os.environ.get("ALLOWED_HOSTS").split(" ")['127.0.0.1', 'https://dev-skill-compass-server.onrender.com']
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
